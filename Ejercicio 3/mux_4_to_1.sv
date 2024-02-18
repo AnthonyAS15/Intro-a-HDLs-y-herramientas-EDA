dce5bca5902e3f74713d80300b5a2d7eee24e9ae
prisc <priscillacamposzamora@gmail.com>
2024-02-17 22:21:40 -0600

    Diseño mux ejercicio 3 terminado.

---
+++ Ejercicio 3/mux_4_to_1.sv
@@ -0,0 +1,7 @@
+// mux 4:1 parametrizado para un ancho de datos "n" de entrada y salida variable
+
+module mux_4_to_1#(parameter n = 4;)(input logic [n-1:0] d0, d1, d2, d3, input logic [1:0] s, output logic [n-1:0] y);
+
+assign y = s[1] ? ( s[0] ? (d3) : (d2) ) : ( s[0] ? (d1) : (d0) );
+
+end module
