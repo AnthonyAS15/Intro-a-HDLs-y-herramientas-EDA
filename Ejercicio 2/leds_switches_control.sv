4cd35baa2ba33c1b4ade14f5b77bea98ae7ac7a5
prisc <priscillacamposzamora@gmail.com>
2024-02-23 14:38:52 -0600

    Correción, faltaban push buttons como entradas.

--- Ejercicio 2/leds_switches_control.sv
+++ Ejercicio 2/leds_switches_control.sv
@@ -2,10 +2,10 @@
 
 `timescale 1ns/1ps
 
-module sw_led(input logic sw0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, sw10, sw11, sw12, sw13, sw14, sw15,
-output logic led0, led1, led2, led3, led4, led5, led6, led7, led8, led9, led10, led11, led12, led13, led14, led15);
-
-logic push_button1, push_button2, push_button3, push_button4;
+module sw_led(input logic sw0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, sw10,
+ sw11, sw12, sw13, sw14, sw15, push_button1, push_button2, push_button3, push_button4, 
+ output logic led0, led1, led2, led3, led4, led5, led6, led7, led8, led9, led10, led11, 
+ led12, led13, led14, led15);
 
 assign led0 = (push_button1) ? 0 : sw0;
 assign led1 = (push_button1) ? 0 : sw1;
