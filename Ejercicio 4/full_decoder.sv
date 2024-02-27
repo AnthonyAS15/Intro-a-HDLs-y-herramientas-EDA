0c468b3c795cda8e4bbbd4315439640a70f0b7f6
prisc <priscillacamposzamora@gmail.com>
2024-02-27 16:05:45 -0600

    Codificador para los switches agregado.

--- Ejercicio 4/full_decoder.sv
+++ Ejercicio 4/full_decoder.sv
@@ -7,7 +7,7 @@ s_d, s_e, s_f, s_g, anode, dp);
 wire d0, d1, d2, d3;
 wire [3:1] d;
 
-switch_decoder sw_deco(.sw0(sw0), .sw1(sw1), .sw2(sw2), .sw3(sw3), .sw4(sw3), .sw5(sw3), .sw6(sw3), .sw7(sw3), .sw8(sw3), 
+switch_encoder sw_encoder(.sw0(sw0), .sw1(sw1), .sw2(sw2), .sw3(sw3), .sw4(sw3), .sw5(sw3), .sw6(sw3), .sw7(sw3), .sw8(sw3), 
 .sw9(sw3), .sw10(sw3), .sw11(sw3), .sw12(sw3), .sw13(sw3), .sw14(sw3), .sw15(sw3), .d0(d0), .d1(d1), .d2(d2), .d3(d3));
 
 mux_4_to_1  #(4) mux(.d0(d0), .d1(d1), .d2(d2), .d3(d3), .s({push_button1, push_button0}), .y(d));
---
+++ Ejercicio 4/switch_decoder.sv
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+module switch_decoder(input logic sw0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, sw10, 
+sw11, sw12, sw13, sw14, sw15, output logic d0, d1, d2, d3);
+
+assign d0 = {sw3, sw2, sw1, sw0};
+assign d1 = {sw7, sw6, sw5, sw4};
+assign d2 = {sw11, sw10, sw9, sw8};
+assign d3 = {sw15, sw14, sw13, sw12};
+
+endmodule
