35a4cb1f8f34447987f6547238f53df39789b3db
AnthonyAS15 <anthonyartaviasalazar@gmail.com>
2024-02-26 21:29:44 -0600

    Salidas independientes y multiplexores.

--- Ejercicio 6/alu.sv
+++ Ejercicio 6/alu.sv
@@ -13,18 +13,49 @@ module alu #(
     output logic [n-1:0] ALUResult,
     output ALUFlagsStruct ALUFlags
 );
+    //Resultados de cada una de las operaciones
+    logic [n-1:0] Result0, Result1, Result2, Result3, Result4, Result5, Result6, Result7, Result8, Result9;
+    //Banderas de salida de cada una de las operaciones
+    ALUFlagsStruct ALUFlags0, ALUFlags1, ALUFlags2, ALUFlags3, ALUFlags4, ALUFlags5, ALUFlags6, ALUFlags7, ALUFlags8, ALUFlags9;
+    //Se馻l de control para el multiplexor que tiene como salida a ALUFlags.C
+    logic [1:0] c_signal;
     
-    Mand #(n) AND(.ALUA(ALUA),.ALUB(ALUB),.ALUResult(ALUResult),.ALUFlags(ALUFlags)); //Operaci贸n l贸gica AND
-    Mor #(n) OR(.ALUA(ALUA),.ALUB(ALUB),.ALUResult(ALUResult),.ALUFlags(ALUFlags)); //Operaci贸n l贸gica OR
-    Madd #(n) ADD(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(ALUResult),.ALUFlags(ALUFlags)); //Suma
-    Minc #(n) INC(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(ALUResult),.ALUFlags(ALUFlags)); //Incrementar en uno
-    Mdec #(n) DEC(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(ALUResult),.ALUFlags(ALUFlags)); //Decrementar en uno
-    Mnot #(n) NOT(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(ALUResult),.ALUFlags(ALUFlags)); //Operaci贸n l贸gica NOT
-    Msub #(n) SUB(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(ALUResult),.ALUFlags(ALUFlags)); //Resta
-    Mxor #(n) XOR (.ALUA(ALUA),.ALUB(ALUB),.ALUResult(ALUResult),.ALUFlags(ALUFlags)); //Operaci贸n l贸gica XOR
-    Msl #(n) SL(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(ALUResult),.ALUFlags(ALUFlags)); //Desplazamiento hacia la izquierda
-    Msr #(n) SR(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(ALUResult),.ALUFlags(ALUFlags)); //Desplazamiento hacia la derecha
-   
+    //M骴ulos de las operaciones
+    Mand #(n) AND(.ALUA(ALUA),.ALUB(ALUB),.ALUResult(Result0),.ALUFlags(ALUFlags0)); //Operaci贸n l贸gica AND
+    Mor #(n) OR(.ALUA(ALUA),.ALUB(ALUB),.ALUResult(Result1),.ALUFlags(ALUFlags1)); //Operaci贸n l贸gica OR
+    Madd #(n) ADD(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(Result2),.ALUFlags(ALUFlags2)); //Suma
+    Minc #(n) INC(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(Result3),.ALUFlags(ALUFlags3)); //Incrementar en uno
+    Mdec #(n) DEC(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(Result4),.ALUFlags(ALUFlags4)); //Decrementar en uno
+    Mnot #(n) NOT(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(Result5),.ALUFlags(ALUFlags5)); //Operaci贸n l贸gica NOT
+    Msub #(n) SUB(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(Result6),.ALUFlags(ALUFlags6)); //Resta
+    Mxor #(n) XOR (.ALUA(ALUA),.ALUB(ALUB),.ALUResult(Result7),.ALUFlags(ALUFlags7)); //Operaci贸n l贸gica XOR
+    Msl #(n) SL(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(Result8),.ALUFlags(ALUFlags8)); //Desplazamiento hacia la izquierda
+    Msr #(n) SR(.ALUA(ALUA),.ALUB(ALUB),.ALUFlagIn(ALUFlagIn),.ALUResult(Result9),.ALUFlags(ALUFlags9)); //Desplazamiento hacia la derecha
+    
+    //Multiplexor para obtener ALUResult
+    mux_10_to_1 #(n) MUXR(.I0(Result0),.I1(Result1),.I2(Result2),.I3(Result3),.I4(Result4),.I5(Result5),.I6(Result6),.I7(Result7),.I8(Result8),.I9(Result9),.ALUControl(ALUControl),.Result(ALUResult));
+    
+    //Multiplexor para obtener ALUFlags.Z
+    mux_10_to_1 #(n) MUXZ(.I0(ALUFlags0.Z),.I1(ALUFlags1.Z),.I2(ALUFlags2.Z),.I3(ALUFlags3.Z),.I4(ALUFlags4.Z),.I5(ALUFlags5.Z),.I6(ALUFlags6.Z),.I7(ALUFlags7.Z),.I8(ALUFlags8.Z),.I9(ALUFlags9.Z),.ALUControl(ALUControl),.Result(ALUFlags.Z));
+    
+    //Determinar la se馻l de control para el multiplexor que tiene como salida a ALUFlags.C
+    always_comb
+    begin
+        case(ALUControl)
+            4'h2:
+                c_signal = 2'b00;
+            4'h6:
+                c_signal = 2'b01;
+            4'h8:
+                c_signal = 2'b10;
+            4'h9:
+                c_signal = 2'b11;
+        endcase
+    end
+    
+    //Multiplexor para obtener ALUFlags.C
+    mux_4_to_1 #(n) MUXC(.d0(ALUFlags2.C),.d1(ALUFlags6.C),.d2(ALUFlags8.C),.d3(ALUFlags9.C),.s(c_signal),.y(ALUFlags.C));
+    
 endmodule
 
 module Mand #( //M贸dulo para hacer AND entre dos entradas
@@ -80,8 +111,10 @@ module Madd #( //M贸dulo para sumar dos entradas y un acarreo
     input logic ALUFlagIn,
     output logic [n-1:0]  ALUResult, 
     output ALUFlagsStruct ALUFlags
-);
-    assign {ALUFlags.C, ALUResult} = ALUA + ALUB + ALUFlagIn;
+);  
+    logic [n:0] sum = ALUA + ALUB + ALUFlagIn;
+    assign ALUFlags.C = sum[n];
+    assign ALUResult = sum[n-1:0];
 
     always_comb
     begin
@@ -206,7 +239,9 @@ module Msub #(
     output logic [n-1:0] ALUResult,
     output ALUFlagsStruct ALUFlags
 );
-    assign {ALUFlags.C, ALUResult} = ALUA - ALUB + ALUFlagIn;
+    logic [n:0] sum = ALUA - ALUB + ALUFlagIn;
+    assign ALUFlags.C = sum[n];
+    assign ALUResult = sum[n-1:0];
 
     always_comb
     begin
