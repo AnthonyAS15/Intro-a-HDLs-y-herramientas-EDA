c79e6dfd3c7194cb8b6e90c0ac6c53d47d750273
prisc <priscillacamposzamora@gmail.com>
2024-02-27 16:24:56 -0600

    Correción en nombre del módulo.

--- Ejercicio 4/switch_encoder.sv
+++ Ejercicio 4/switch_encoder.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module switch_decoder(input logic sw0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, sw10, 
+module switch_encoder(input logic sw0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, sw10, 
 sw11, sw12, sw13, sw14, sw15, output logic d0, d1, d2, d3);
 
 assign d0 = {sw3, sw2, sw1, sw0};
