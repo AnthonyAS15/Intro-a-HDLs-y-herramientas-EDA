9aa84ae7bc332081ffaf2e67659e251ef98572a4
prisc <priscillacamposzamora@gmail.com>
2024-02-27 17:32:09 -0600

    Corrección error núm. de bits de salidas de 1 a 4.

--- Ejercicio 4/switch_encoder.sv
+++ Ejercicio 4/switch_encoder.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 module switch_encoder(input logic sw0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, sw10, 
-sw11, sw12, sw13, sw14, sw15, output logic d0, d1, d2, d3);
+sw11, sw12, sw13, sw14, sw15, output logic [3:0] d0, d1, d2, d3);
 
 assign d0 = {sw3, sw2, sw1, sw0};
 assign d1 = {sw7, sw6, sw5, sw4};
