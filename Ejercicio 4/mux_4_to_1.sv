6e2515cbb297622cc93cdd2adc19585057640102
prisc <priscillacamposzamora@gmail.com>
2024-02-27 17:30:49 -0600

    Desparametrización del mux. Que fijo de 4 bits.

--- Ejercicio 4/mux_4_to_1.sv
+++ Ejercicio 4/mux_4_to_1.sv
@@ -2,7 +2,7 @@
 
 `timescale 1ns/1ps
 
-module mux_4_to_1#(parameter n = 4)(input logic [n-1:0] d0, d1, d2, d3, input logic [1:0] s, output logic [n-1:0] y);
+module mux_4_to_1(input logic [3:0] d0, d1, d2, d3, input logic [1:0] s, output logic [3:0] y);
 
 assign y = s[1] ? ( s[0] ? (d3) : (d2) ) : ( s[0] ? (d1) : (d0) );
 
